95553a1efea8d7829b82ae46efff09b05c270c3c
ENT20111265\SAAD-IFTIKHAR <saad.i@theentertainerasia.com>
2023-04-08 23:12:32 +0500

    update version and prepare layout for all toasts

--- app/src/main/java/com/saadiftkhar/toaster/MainActivity.kt
+++ app/src/main/java/com/saadiftkhar/toaster/MainActivity.kt
@@ -2,11 +2,27 @@ package com.saadiftkhar.toaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toaster.success(this, "hello")
+
+        findViewById<Button>(R.id.btnSuccess).setOnClickListener {
+            Toaster.success(this, "success")
+        }
+        findViewById<Button>(R.id.btnError).setOnClickListener {
+            Toaster.error(this, "error")
+        }
+        findViewById<Button>(R.id.btnDelete).setOnClickListener {
+            Toaster.delete(this, "delete")
+        }
+        findViewById<Button>(R.id.btnWarning).setOnClickListener {
+            Toaster.warning(this, "warning")
+        }
+        findViewById<Button>(R.id.btnInfo).setOnClickListener {
+            Toaster.info(this, "info")
+        }
     }
 }
